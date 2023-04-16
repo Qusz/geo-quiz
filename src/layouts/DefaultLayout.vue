af3fb737311528f5de78b8f07116ac19346f7e30
Qusz <de.gorchakov@gmail.com>
2023-04-16 18:44:15 +0300

    add default layout

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="default-layout">
+    <header class="default-layout__header">
+      <TheNavbar></TheNavbar>
+    </header>
+    <main class="default-layout__content">
+      <slot></slot>
+    </main>
+    <TheFooter class="default-layout__footer"></TheFooter>
+  </div>
+</template>
+
+<script setup lang="ts">
+import TheNavbar from 'components/navbar/TheNavbar.vue';
+import TheFooter from 'components/footer/TheFooter.vue';
+</script>
+
+<style scoped lang="scss">
+@import 'assets/scss/layouts/default-layout';
+</style>
