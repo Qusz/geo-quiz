874be739c6ba7fd5dd26ff97b2d1a4ac5bf0f345
Qusz <de.gorchakov@gmail.com>
2023-04-17 21:35:37 +0300

    add component

---
+++ src/components/common/BrandLogo.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import BrandLogoImage from './BrandLogoImage.vue';
+</script>
+
+<template>
+  <div class="brand-logo">
+    <div class="brand-logo__wrapper">
+      <brand-logo-image class="brand-logo__image"></brand-logo-image>
+      <span class="brand-logo__text">GeoQuiz</span>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import 'assets/scss/components/common/BrandLogo';
+</style>
