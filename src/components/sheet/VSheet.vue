15e3b241e00deda801761421e5fa5e87ac6c03f5
Qusz <de.gorchakov@gmail.com>
2023-04-17 21:36:53 +0300

    add component

---
+++ src/components/sheet/VSheet.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const props = defineProps<{
+  fluid?: boolean;
+}>();
+</script>
+
+<template>
+  <div :class="['sheet', { 'sheet--fluid': fluid }]">
+    <div class="sheet__wrapper">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import 'assets/scss/components/sheet/VSheet';
+</style>
