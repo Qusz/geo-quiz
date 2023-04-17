8e93c73c6eed3d49d2e83e0765236d4ddeb9140e
Qusz <de.gorchakov@gmail.com>
2023-04-17 21:36:10 +0300

    add component

---
+++ src/components/footer/FooterCopyright.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="footer-copyright">
+    <p class="footer-copyright__caption">
+      Developed by
+      <a class="footer-copyright__link" href="https://github.com/Qusz" target="_blank">
+        Denis Gorchakov
+      </a>
+    </p>
+  </section>
+</template>
+
+<style scoped lang="scss">
+@import 'assets/scss/components/footer/FooterCopyright';
+</style>
