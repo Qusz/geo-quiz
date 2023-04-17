2268d1ef2f39ad1fbbc063b7ade0388ac9b3a29c
Qusz <de.gorchakov@gmail.com>
2023-04-17 21:36:36 +0300

    add component

---
+++ src/components/navbar/NavbarItems.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import type { NavbarItemType } from '@/types';
+import NavbarItem from './NavbarItem.vue';
+
+const navbarItems: NavbarItemType[] = [
+  {
+    name: 'New Game',
+    route: 'NewGame',
+    tag: 'div',
+    buttonColor: 'green'
+  },
+  {
+    name: 'Your Stats',
+    route: 'PlayerStats',
+    tag: 'div',
+    buttonShape: 'plain'
+  }
+];
+</script>
+
+<template>
+  <ul class="navbar-items">
+    <navbar-item
+      v-for="(item, index) in navbarItems"
+      :key="index"
+      :page-name="item.route"
+      :button-tag="item.tag"
+      :button-color="item.buttonColor"
+      :button-shape="item.buttonShape"
+    >
+      {{ item.name }}
+    </navbar-item>
+  </ul>
+</template>
+
+<style scoped lang="scss">
+@import 'assets/scss/components/navbar/NavbarItems';
+</style>
