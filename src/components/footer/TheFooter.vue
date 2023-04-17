77033e648d0d7c33b9ef52df18f9c6de281d5f86
Qusz <de.gorchakov@gmail.com>
2023-04-17 21:36:19 +0300

    add component

---
+++ src/components/footer/TheFooter.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import FooterCopyright from './FooterCopyright.vue';
+</script>
+
+<template>
+  <footer class="footer">
+    <div class="footer__wrapper container">
+      <footer-copyright></footer-copyright>
+    </div>
+  </footer>
+</template>
+
+<style scoped lang="scss">
+@import 'assets/scss/components/footer/TheFooter';
+</style>
