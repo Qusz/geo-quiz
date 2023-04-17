f29a9009913f585a441d7224e79f7a91fb8577cd
Qusz <de.gorchakov@gmail.com>
2023-04-17 21:35:44 +0300

    add component

---
+++ src/components/common/BrandLogoImage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="brand-logo-image">
+    <div class="brand-logo-image__wrapper">
+      <img src="/logo.png" alt="logo" class="brand-logo-image__image" />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import 'assets/scss/components/common/BrandLogoImage';
+</style>
