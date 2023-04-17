09f8bb2607b37e8685390265ab542e334ca0a2a4
Qusz <de.gorchakov@gmail.com>
2023-04-17 21:36:44 +0300

    add component

---
+++ src/components/navbar/TheNavbar.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import BrandLogo from 'components/common/BrandLogo.vue';
+import NavbarItems from './NavbarItems.vue';
+</script>
+
+<template>
+  <nav class="navbar">
+    <div class="navbar__wrapper container">
+      <router-link :to="{ name: 'Home' }">
+        <brand-logo class="navbar__logo"></brand-logo>
+      </router-link>
+      <navbar-items></navbar-items>
+    </div>
+  </nav>
+</template>
+
+<style scoped lang="scss">
+@import 'assets/scss/components/navbar/TheNavbar';
+</style>
