5febe11aa1ddffdfe81decc9e6bb9e429ecd3f90
Qusz <de.gorchakov@gmail.com>
2023-04-17 21:35:59 +0300

    add component

---
+++ src/components/common/VButton.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import type { ButtonColor, ButtonShape, ButtonType, ButtonTag } from '@/types';
+
+const props = defineProps<{
+  tag: ButtonTag;
+  color?: ButtonColor;
+  shape?: ButtonShape;
+  type?: ButtonType;
+}>();
+</script>
+
+<template>
+  <component
+    :is="tag"
+    :class="[
+      'button',
+      {
+        'button--green': color === 'green',
+        'button--white': color === 'white',
+        'button--sienna': color === 'sienna',
+        'button--plain': shape === 'plain'
+      }
+    ]"
+    :type="type"
+  >
+    <div class="button__wrapper">
+      <slot></slot>
+    </div>
+  </component>
+</template>
+
+<style scoped lang="scss">
+@import 'assets/scss/components/common/VButton';
+</style>
