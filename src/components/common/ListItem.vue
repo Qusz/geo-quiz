8860eb27ab329aed4c10fcf51f49356e639c329a
Qusz <de.gorchakov@gmail.com>
2023-04-19 07:26:25 +0300

    add component

---
+++ src/components/common/ListItem.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { ListItemTag } from '@/types';
+
+const props = defineProps<{
+  tag: ListItemTag;
+}>();
+</script>
+
+<template>
+  <component :is="tag" class="list-item">
+    <slot></slot>
+  </component>
+</template>
+
+<style scoped lang="scss">
+@import 'assets/scss/components/common/ListItem';
+</style>
