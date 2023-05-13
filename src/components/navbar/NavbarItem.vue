f7677095b68bae2ec2bf6230e245b010f04d2c42
Qusz <de.gorchakov@gmail.com>
2023-05-13 21:04:17 +0300

    remove router link

--- src/components/navbar/NavbarItem.vue
+++ src/components/navbar/NavbarItem.vue
@@ -1,12 +1,9 @@
 <script setup lang="ts">
-import { RouterLink } from 'vue-router';
 import VButton from 'components/common/VButton.vue';
 
 import type { ButtonColor, ButtonTag, ButtonShape, ButtonType } from '@/types';
 
 const props = defineProps<{
-  // TODO create a type for router links
-  pageName: string;
   buttonTag: ButtonTag;
   buttonColor?: ButtonColor;
   buttonShape?: ButtonShape;
@@ -16,11 +13,9 @@ const props = defineProps<{
 
 <template>
   <li class="navbar-item">
-    <router-link :to="{ name: pageName }">
-      <v-button :tag="buttonTag" :color="buttonColor" :shape="buttonShape">
-        <slot></slot>
-      </v-button>
-    </router-link>
+    <v-button :tag="buttonTag" :color="buttonColor" :shape="buttonShape">
+      <slot></slot>
+    </v-button>
   </li>
 </template>
 
