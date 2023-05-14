b8e27b4af368f328a4eada6f38833a2425b5a02e
Qusz <de.gorchakov@gmail.com>
2023-05-14 08:12:41 +0300

    add component

---
+++ src/components/game-interface/GameInterfacePlay.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import GameInterfaceOptions from './GameInterfaceOptions.vue';
+import GameInterfaceQuestion from './GameInterfaceQuestion.vue';
+</script>
+
+<template>
+  <div class="game-interface-play">
+    <game-interface-question></game-interface-question>
+    <game-interface-options></game-interface-options>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
