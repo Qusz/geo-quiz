31f71affb3fca448d7364f3dcca19ba07f531e07
Qusz <de.gorchakov@gmail.com>
2023-05-14 08:11:35 +0300

    add app state core logic

---
+++ src/stores/app-state.ts
@@ -0,0 +1,84 @@
+import type { AppState, GameMode } from '@/types';
+import type { Ref } from 'vue';
+
+import { ref } from 'vue';
+import { defineStore } from 'pinia';
+
+import Routes from 'utils/router-push';
+
+export const useAppState = defineStore('app-state', () => {
+  const state: Ref<AppState> = ref('default');
+  const gameMode: Ref<GameMode> = ref(undefined);
+
+  const routes = new Routes();
+
+  const toMenu = () => {
+    state.value = 'select-menu';
+    routes.toMainMenu();
+  };
+
+  const startGame = (mode: GameMode) => {
+    if (state.value === 'play') return;
+    state.value = 'play';
+
+    try {
+      switch (mode) {
+        case 'flag-by-country':
+          gameMode.value = 'flag-by-country';
+          routes.toFlagByCountry();
+          break;
+        case 'country-by-flag':
+          gameMode.value = 'country-by-flag';
+          routes.toCountryByFlag();
+          break;
+        case 'capital-by-country':
+          gameMode.value = 'capital-by-country';
+          routes.toCapitalByCountry();
+          break;
+        case 'country-by-capital':
+          gameMode.value = 'country-by-capital';
+          routes.toCountryByCapital();
+          break;
+        case undefined:
+          break;
+        default:
+          throw new Error(`Unexpected value of gameMode: ${gameMode.value}`);
+      }
+    } catch (error) {
+      // TODO add error handling
+      console.log(error);
+    }
+  };
+
+  const loseGame = () => {
+    if (state.value !== 'lose') {
+      state.value = 'lose';
+    }
+  };
+
+  const winGame = () => {
+    if (state.value !== 'win') {
+      state.value = 'win';
+    }
+  };
+
+  const gameModeReset = () => {
+    gameMode.value = undefined;
+  };
+
+  const $reset = () => {
+    state.value = 'default';
+    gameModeReset();
+  };
+
+  return {
+    state,
+    gameMode,
+    toMenu,
+    startGame,
+    winGame,
+    loseGame,
+    gameModeReset,
+    $reset
+  };
+});
