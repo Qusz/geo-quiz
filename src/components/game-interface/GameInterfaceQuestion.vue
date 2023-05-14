a0c8a04cc19028105e8f46e8fd0b5f2399c254d1
Qusz <de.gorchakov@gmail.com>
2023-05-14 08:12:32 +0300

    add component

---
+++ src/components/game-interface/GameInterfaceQuestion.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { useAppState } from '@/stores/app-state';
+import { useGameplayControl } from '@/stores/gameplay-control';
+
+const appState = useAppState();
+const gameplayControl = useGameplayControl();
+</script>
+
+<template>
+  <div class="game-interface-question">
+    <div v-if="appState.gameMode === 'flag-by-country'" class="game-interface-question__text">
+      <p>Which one is the flag of {{ gameplayControl.question }}?</p>
+    </div>
+
+    <div v-else-if="appState.gameMode === 'country-by-flag'" class="game-interface-question__flag">
+      <img :src="gameplayControl.flagUrl" alt="flag" class="game-interface-question__flag-img" />
+    </div>
+
+    <div v-else class="game-interface-question__text">
+      <p>{{ gameplayControl.question }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import 'assets/scss/components/game-interface/GameInterfaceQuestion';
+</style>
