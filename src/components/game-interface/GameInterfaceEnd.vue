6eea0e438e1a9f1b431813ed04b94900a2350b67
Qusz <de.gorchakov@gmail.com>
2023-05-14 08:12:15 +0300

    add component

---
+++ src/components/game-interface/GameInterfaceEnd.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { useGameplayControl } from '@/stores/gameplay-control';
+import { useAppState } from '@/stores/app-state';
+
+const appState = useAppState();
+const gameplayControl = useGameplayControl();
+</script>
+
+<template>
+  <div class="game-interface-end">
+    <div class="game-interface-end__wrapper">
+      <div v-if="appState.state === 'lose'" class="game-interface-end__item">
+        <p class="game-interface-end__message">You lost :/</p>
+
+        <p
+          v-if="appState.gameMode === 'country-by-flag' || appState.gameMode === 'flag-by-country'"
+          class="game-interface-end__result"
+        >
+          Your guessed {{ gameplayControl.correctAnswers }} flags.
+        </p>
+
+        <p
+          v-else-if="appState.gameMode === 'capital-by-country'"
+          class="game-interface-end__result"
+        >
+          Your guessed {{ gameplayControl.correctAnswers }} capitals.
+        </p>
+
+        <p v-else class="game-interface-end__result">
+          Your guessed {{ gameplayControl.correctAnswers }} countries.
+        </p>
+      </div>
+
+      <div v-else class="game-interface-end__item">
+        <p class="game-interface-end__message">Congratulations! You've guessed them all!</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import '@/assets/scss/components/game-interface/GameInterfaceEnd';
+</style>
