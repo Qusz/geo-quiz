a956cbadc3893df4f0edd24460f926f982ba3cf7
Qusz <de.gorchakov@gmail.com>
2023-05-14 08:22:45 +0300

    remove redundant paths

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,31 +14,6 @@ const router = createRouter({
       path: '/new-game',
       name: 'NewGame',
       component: () => import('views/NewGame.vue')
-    },
-    {
-      path: '/capitbal-by-coutry',
-      name: 'CapitalByCountry',
-      component: () => import('views/CapitalByCountry.vue')
-    },
-    {
-      path: '/country-by-capital',
-      name: 'CountryByCapital',
-      component: () => import('views/CountryByCapital.vue')
-    },
-    {
-      path: '/country-by-flag',
-      name: 'CountryByFlag',
-      component: () => import('views/CountryByFlag.vue')
-    },
-    {
-      path: '/flag-by-country',
-      name: 'FlagByCountry',
-      component: () => import('views/FlagByCountry.vue')
-    },
-    {
-      path: '/stats',
-      name: 'PlayerStats',
-      component: () => import('views/PlayerStats.vue')
     }
   ]
 });
