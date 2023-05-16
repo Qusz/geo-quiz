544e1657ea58a0ab31cfa22e330dc09ce9aea2d8
Qusz <de.gorchakov@gmail.com>
2023-05-16 22:11:21 +0300

    update game modes

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,8 +1,3 @@
 import type { GameMode } from '@/types';
 
-export const GAME_MODES: GameMode[] = [
-  'country-by-flag',
-  'flag-by-country',
-  'capital-by-country',
-  'country-by-capital'
-];
+export const GAME_MODES: GameMode[] = ['country-by-flag', 'flag-by-country'];
