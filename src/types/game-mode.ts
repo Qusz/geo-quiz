b86dedbd6abc79fa1e49e87e7639fca8a9c3f078
Qusz <de.gorchakov@gmail.com>
2023-05-16 22:07:38 +0300

    update type

--- src/types/game-mode.ts
+++ src/types/game-mode.ts
@@ -1,6 +1,1 @@
-export type GameMode =
-  | 'flag-by-country'
-  | 'country-by-flag'
-  | 'capital-by-country'
-  | 'country-by-capital'
-  | undefined;
+export type GameMode = 'flag-by-country' | 'country-by-flag' | undefined;
