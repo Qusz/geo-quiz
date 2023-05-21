e8cb13495080efd5e375cf11364c3a0eb1b2de52
Qusz <de.gorchakov@gmail.com>
2023-05-21 14:15:18 +0300

    set up Toastify

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,23 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
+import Vue3Toasity, { type ToastContainerOptions } from 'vue3-toastify';
+
 import App from './App.vue';
 import router from './router';
 
 import 'normalize.css';
+import 'vue3-toastify/dist/index.css';
 import 'assets/scss/main.scss';
 
 const app = createApp(App);
+
+app.use(Vue3Toasity, {
+  multiple: false,
+  position: 'top-center',
+  hideProgressBar: true,
+  theme: 'colored'
+} as ToastContainerOptions);
 
 app.use(createPinia());
 app.use(router);
