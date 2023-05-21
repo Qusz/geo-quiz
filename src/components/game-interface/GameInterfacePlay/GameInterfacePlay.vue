2cca2847b5529f798f4a86aaf60ae7cf6db0a151
Qusz <de.gorchakov@gmail.com>
2023-05-21 15:07:19 +0300

    add spinner

--- src/components/game-interface/GameInterfacePlay/GameInterfacePlay.vue
+++ src/components/game-interface/GameInterfacePlay/GameInterfacePlay.vue
@@ -1,13 +1,27 @@
 <script setup lang="ts">
+import { useGameplayControl } from '@/stores/gameplay-control';
+
+import VSpinner from '@/components/common/VSpinner/VSpinner.vue';
 import GameInterfaceOptions from '../GameInterfaceOptions/GameInterfaceOptions.vue';
 import GameInterfaceQuestion from '../GameInterfaceQuestion/GameInterfaceQuestion.vue';
+
+const gameplayControl = useGameplayControl();
 </script>
 
 <template>
   <div class="game-interface-play">
-    <game-interface-question></game-interface-question>
-    <game-interface-options></game-interface-options>
+    <div class="game-interface-play__wrapper">
+      <div v-if="!gameplayControl.gameData.length" class="game-interface-play__spinner">
+        <v-spinner></v-spinner>
+      </div>
+      <div v-else>
+        <game-interface-question></game-interface-question>
+        <game-interface-options></game-interface-options>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@import './GameInterfacePlay.scss';
+</style>
